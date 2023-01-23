0070727cc5a4123efa169815f8c6b021ff4ac126
RobinAlfengård <robin.alfengard@hotmail.com>
2023-01-23 17:33:34 +0100

    added getter and setters

--- src/Books.java
+++ src/Books.java
@@ -1,5 +1,34 @@
 public class Books {
     public String title;
     public String author;
+
     int price;
-}
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+
+
+
+    }
+
--- src/Movies.java
+++ src/Movies.java
@@ -3,6 +3,31 @@ import java.rmi.MarshalledObject;
 public class Movies {
     String title;
     MovieGenres genre;
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public void setGenre(MovieGenres genre) {
+        this.genre = genre;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public MovieGenres getGenre() {
+        return genre;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
     int price;
 
 }
