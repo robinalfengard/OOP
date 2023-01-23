25f8d3fc053b01d510434537024650ceba61dd22
RobinAlfengård <robin.alfengard@hotmail.com>
2023-01-23 17:23:24 +0100

    added methods for movies and books

--- src/Main.java
+++ src/Main.java
@@ -1,25 +1,41 @@
 public class Main {
     public static void main(String[] args) {
-        Books[] book = new Books[2];
+        Books[] bookShelf = new Books[2];
+        Movies[] moovieShelf = new Movies[2];
         Books book1 = new Books();
         Books book2 = new Books();
+        bookShelf[0] = book1;
+        bookShelf[1] = book2;
         book1.title = "Harry Potter";
         book1.author = "JK Rowling";
         book1.price = 150;
         book2.title = "IT";
         book2.author = "Stephen King";
         book2.price = 100;
-        printInfoBook(book1);
+
 
         Movies movie1 = new Movies();
+        Movies movie2 = new Movies();
         movie1.title = "James Bond";
         movie1.genre = MovieGenres.ACTION;
         movie1.price = 150;
-        printInfoMovie(movie1);
+        movie2.title = "American Psycho";
+        movie2.genre = MovieGenres.ACTION;
+        movie2.price = 150;
+        moovieShelf[0] = movie1;
+        moovieShelf[1] = movie2;
 
+
+
+        for (Books book : bookShelf){
+            printInfoBook(book);
+        }
+        for (int i = 0; i<moovieShelf.length; i++){
+            printInfoMovie(moovieShelf[i]);
+        }
     }
 
-    static void printInfoBook(Books book){
+    private static void printInfoBook(Books book){
         System.out.println("Book Title: " + book.title + "\nAuthor: " + book.author + "\n Price: " + book.price );
     }
     static void printInfoMovie(Movies movie){
